e767e23c16314fcaadf68324db70fe22a95a60b2
FireSonic <prostouser89@gmail.com>
2024-08-23 00:31:11 +0300

    Created repositories for main entities.

---
+++ src/main/java/com/currency/repositories/ExchangeRateRepository.java
@@ -0,0 +1,9 @@
+package com.currency.repositories;
+
+import com.currency.models.ExchangeRate;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ExchangeRateRepository extends JpaRepository<ExchangeRate, Long> {
+}
