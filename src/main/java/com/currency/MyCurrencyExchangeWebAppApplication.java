48d145c86fa087707b9ea8db5be91bb7bbbd8b9c
FireSonic <prostouser89@gmail.com>
2024-08-23 00:06:10 +0300

    Start

---
+++ src/main/java/com/currency/MyCurrencyExchangeWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.currency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCurrencyExchangeWebAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyCurrencyExchangeWebAppApplication.class, args);
+    }
+
+}
