b2a7a384ff4158036c36dd7069d9eb825ebccef1
FireSonic <prostouser89@gmail.com>
2024-08-24 03:14:40 +0300

    Created two methods. -To get a single currency by its code. -To add new currency.

--- src/main/java/com/currency/services/CurrencyService.java
+++ src/main/java/com/currency/services/CurrencyService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @Transactional(readOnly = true)
@@ -21,6 +22,15 @@ public class CurrencyService {
 
     public List<Currency> getCurrencies() {
         return currencyRepository.findAll();
+    }
+
+    public Optional<Currency> getCurrency(String sign) {
+        return currencyRepository.findCurrenciesByCode(sign);
+    }
+
+    @Transactional(readOnly = false)
+    public Currency addCurrency(Currency currency) {
+        return currencyRepository.save(currency);
     }
 
 }
