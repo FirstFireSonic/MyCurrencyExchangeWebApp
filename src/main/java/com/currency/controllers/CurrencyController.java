bb585735cc8c2f60eae28b3f954133ab12e30efe
FireSonic <prostouser89@gmail.com>
2024-08-24 03:15:39 +0300

    -Changed endpoints names. -Created addNewCurrency controller.

--- src/main/java/com/currency/controllers/CurrencyController.java
+++ src/main/java/com/currency/controllers/CurrencyController.java
@@ -5,12 +5,15 @@ import com.currency.models.ExchangeRate;
 import com.currency.services.CurrencyService;
 import com.currency.services.ExchangeRateService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
+@RequestMapping("/currency")
 public class CurrencyController {
 
     private CurrencyService currencyService;
@@ -22,14 +25,29 @@ public class CurrencyController {
         this.exchangeRateService = exchangeRateService;
     }
 
-    @GetMapping("/currencies")
+    @GetMapping("/all")
     public List<Currency> getAllAvailableCurrencies() {
         return currencyService.getCurrencies();
     }
 
-    @GetMapping("/exchange-rate")
-    public List<ExchangeRate> getAllAvailableExchangeRate() {
-        return exchangeRateService.getAllExchangeRates();
+    @GetMapping("/{sign}")
+    public Optional<Currency> getCurrency(@PathVariable String sign) {
+        return currencyService.getCurrency(sign);
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<Currency> addNewCurrency(
+            @RequestParam("name") String name,
+            @RequestParam("code") String code,
+            @RequestParam("sign") String sign) {
+        Currency savedCurrency = currencyService.addCurrency(
+                Currency.builder()
+                        .code(code)
+                        .fullName(name)
+                        .sign(sign)
+                        .build()
+        );
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedCurrency);
     }
 
 }
