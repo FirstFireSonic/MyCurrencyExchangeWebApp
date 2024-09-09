db6dcb694a0f265c16ce0886e89ff530896e579c
FireSonic <prostouser89@gmail.com>
2024-09-09 15:51:47 +0200

    Refactoring

--- src/main/java/com/currency/mapper/CertainExchangeRateDTOMapper.java
+++ src/main/java/com/currency/mapper/CertainExchangeRateDTOMapper.java
@@ -5,11 +5,10 @@ import com.currency.dto.CurrencyDTO;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
-import java.util.Optional;
 
 @Service
 public class CertainExchangeRateDTOMapper {
-    public CertainExchangeRateDTO mapToDTO(CurrencyDTO baseCurrency, CurrencyDTO targetCurrency, BigDecimal rate) {
+    public CertainExchangeRateDTO apply(CurrencyDTO baseCurrency, CurrencyDTO targetCurrency, BigDecimal rate) {
         return new CertainExchangeRateDTO(
                 baseCurrency,
                 targetCurrency,
